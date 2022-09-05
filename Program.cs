3dcc3c5e1bf72a189334566ae05daf3d6aea69b5
Harlit <100050409+Harlit@users.noreply.github.com>
2022-09-05 15:30:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Harlit Enmanuel De La Cruz Estevez");
+Console.WriteLine("Hola al harlit del 2022") // error a proposito a ver como se comporta el compilador
+Console.WriteLine("Hola Universo :)"); // El HOLA MUNDO SE USA MUCHO HAHAHA
